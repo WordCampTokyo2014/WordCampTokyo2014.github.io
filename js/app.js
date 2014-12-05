12425765d05b65efc3cb6154cdc2296e16228b48
森山 真祐子 <mayuko.jpn@gmail.com>
2014-12-05 11:45:46 +0900

    Update add thubnail.

--- js/app.js
+++ js/app.js
@@ -30,23 +30,25 @@ function create_repo_div(json) {
       var div = document.createElement("div"),
       title = document.createElement("h2");
 
-      // --- It's for debug. Remove it once you create thumbnail function --- //
-      if(json.name=="logo"){
-	var img = new Image();
-        img.src = 'https://raw.githubusercontent.com/' + json.full_name + '/master/PNG/logo_a.png';
-	//img.style.setAttribute('max-width','100%');
-	img.style.cssText='max-width: 100%';
-        div.appendChild(img);
-      }
+      // --- Add thumbnail image ---- //
+      var img = new Image();
+      img.src = 'https://raw.githubusercontent.com/' + json.full_name + '/master/sample/thumb.png';
+      img.style.cssText='max-width: 100%';
+      img.setAttribute('onerror','imgError(this);');
+      div.appendChild(img);
 
+      // --- Add repository name ---- //
       title.innerHTML += '<a href="' + json.html_url + '">' + json.name,'</a>';
       div.appendChild(title);
 
+      // --- Add description ---- //
       if(json.description){
         var p = document.createElement("p");
         p.innerHTML += json.description;
         div.appendChild(p);
       }
+
+      // --- Add update date ---- //
       if(json.updated_at){
         var p = document.createElement("p");
         p.innerHTML += "Updated at " + json.updated_at.substr(0, 10);
@@ -54,4 +56,11 @@ function create_repo_div(json) {
       }
 
       return div;
+}
+
+function imgError(img) {
+    img.onerror = "";
+    img.src = "http://placehold.it/300x60/ef86b4/fff&text=Add+thumb.png";
+    //img.style.cssText='width: 150px';
+    return true;
 }
